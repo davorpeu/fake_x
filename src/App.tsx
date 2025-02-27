14d7f6475dc71b1d34bd0d26a07e255351c366ad
Davor Peuraca <davorpeu@gmail.com~>
2025-02-27 14:40:36 +0100

    gh pages setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes';
 import './App.css'; // Adjust the path as needed
+
 function App() {
-  return (
-      <BrowserRouter>
+    return (
         <AppRoutes />
-      </BrowserRouter>
-  );
+    );
 }
 
 export default App;
